6565318d1d69aef1b7901bf767a0c950c1e5e032
Andrei <korovinn96@gmail.com>
2024-01-10 18:39:51 +0300

    add ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
 
 import "./styles/index.scss";
 
 const App = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () =>
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
