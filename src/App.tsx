12cb889ba8403d1554e4911d58cb000c3e5bebd5
Andrei <korovinn96@gmail.com>
2024-01-10 18:48:05 +0300

    add useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { Suspense, useContext, useState } from "react";
+import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 import "./styles/index.scss";
 
 const App = () => {
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  const toggleTheme = () =>
-    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
-
+  const { theme, toggleTheme } = useTheme();
   return (
     <div className={`app ${theme}`}>
       <button onClick={toggleTheme}>Toggle</button>
